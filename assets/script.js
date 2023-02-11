b155b095dd7e461f8dfbb0a710604b86c2366134
Alex Marriott <alexdcmarriott@gmail.com>
2023-02-11 14:48:51 -0800

    Played with prompts/window confirm to get popups working.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,37 @@
 // Assignment code here
+var passwordLength = parseInt(prompt("Enter password length (min 8 characters):"), 10);
+
+// var (passwordLength < 8) {
+//     passwordLength = parseInt(prompt("Password length must be at least 8 characters. Enter password length:"));
+// };
+
+
+var passgen = confirm("Would you like to generate a password?");
+var numbers = confirm("Would you like numbers?");
+var specChars = confirm("Would you like special characters?");
+var lwrcase = confirm("Would you like lower case?");
+var uprcase = confirm("Would you like upercase?");
+
+var characterset = "";
+
+if (numbers) {
+  characterset += "1234567890";
+}
+
+if (specChars) {
+  characterset += "!@#$%^&*()_+-=[]{}|;':\",.<>/?\\";
+}
+
+if (lwrcase) {
+  characterset += "abcdefghijklmnopqrstuvwxyz";
+}
+
+if (uprcase) {
+  characterset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+
+
 
 
 // Get references to the #generate element
