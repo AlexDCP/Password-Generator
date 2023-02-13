2f348284de049d3ff021305f2cb2812b965502cb
AlexDCP <alexdcmarriot@gmail.com>
2023-02-13 13:28:11 -0800

    Finished JavaScript section, will finish readme next

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,54 @@
 // Assignment code here
-var passwordLength = parseInt(prompt("Enter password length (min 8 characters):"), 10);
+var passwordLength;
+passwordLength = parseInt(prompt("Enter password length (min 8 characters):"), 10);
 
-// var (passwordLength < 8) {
-//     passwordLength = parseInt(prompt("Password length must be at least 8 characters. Enter password length:"));
-// };
+if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt("Password length must be at least 8 characters, and no greater than 128. Enter password length:"));
+};
+
+
 
 
 var passgen = confirm("Would you like to generate a password?");
 var numbers = confirm("Would you like numbers?");
 var specChars = confirm("Would you like special characters?");
 var lwrcase = confirm("Would you like lower case?");
-var uprcase = confirm("Would you like upercase?");
+var uprcase = confirm("Would you like uppercase?");
 
-var characterset = "";
+var characterSet = "";
 
 if (numbers) {
-  characterset += "1234567890";
+  characterSet += "1234567890";
 }
 
 if (specChars) {
-  characterset += "!@#$%^&*()_+-=[]{}|;':\",.<>/?\\";
+  characterSet += "!@#$%^&*()_+-=[]{}|;':\",.<>/?\\";
 }
 
 if (lwrcase) {
-  characterset += "abcdefghijklmnopqrstuvwxyz";
+  characterSet += "abcdefghijklmnopqrstuvwxyz";
 }
 
 if (uprcase) {
-  characterset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  characterSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 }
 
 
+function generatePassword() {
+  var splitCharacterSet = characterSet.split("");
+  var generatedPassword = "";
 
+  console.log(splitCharacterSet);
+
+  for (var i = 0; i < passwordLength; i++){
+    var randomNum = Math.floor(Math.random() * splitCharacterSet.length);
+
+    generatedPassword += splitCharacterSet[randomNum]
+    // console.log(i)
+  };
+
+  return generatedPassword;
+};
 
 
 // Get references to the #generate element
@@ -43,7 +60,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  console.log(password)
 }
 
 // Add event listener to generate button
